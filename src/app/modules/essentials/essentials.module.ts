537dac0997ebb371b2087d13eb6081d977d694d3
darlington10 <darlingtonokiemute!@gmail.com>
2023-12-19 13:25:02 +0100

    Prepare assignment template

---
+++ src/app/modules/essentials/essentials.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ButtonsModule } from 'nextsapien-component-lib';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, ButtonsModule],
+  exports: [ButtonsModule],
+})
+export class EssentialsModule {}
